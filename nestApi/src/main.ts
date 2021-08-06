903fa368e28e51b2f6da3fbc4dc177cf05ad3ced
Andrew Kozinsky <andkozinskiy@yandex.ru>
2021-08-06 06:24:18 +0500

    Trying to run the code in server 9

--- nestApi/src/main.ts
+++ nestApi/src/main.ts
@@ -6,7 +6,6 @@ import { config } from './config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
-  // app.setGlobalPrefix('api2')
   await app.listen(config.port)
 }
 bootstrap()
