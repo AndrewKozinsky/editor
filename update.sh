419b474e2206cb1145b55ce8bd12a88e621b172c
Andrew Kozinsky <andkozinskiy@yandex.ru>
2022-03-07 07:41:00 +0300

    Fix errors

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
-docker-compose -f docker-compose-server.yml down
 git pull
+docker-compose -f docker-compose-server.yml down
+docker build -t editor-next ./next/Dockerfile.prod
 docker-compose -f docker-compose-server.yml up
